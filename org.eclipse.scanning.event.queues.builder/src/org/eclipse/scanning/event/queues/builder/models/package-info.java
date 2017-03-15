1a567aed50a254a0f637daf5312c339160f38677
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2017-03-15 17:37:08 +0000

    Adds new plugin for the queue builder.
    Signed-off-by: Michael Wharmby <michael.wharmby@diamond.ac.uk>

---
+++ org.eclipse.scanning.event.queues.builder/src/org/eclipse/scanning/event/queues/builder/models/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package containing the models used as templates for the different sorts of 
+ * queue beans.
+ * 
+ * @author Michael Wharmby
+ *
+ */
+package org.eclipse.scanning.event.queues.builder.models;
---
+++ org.eclipse.scanning.event.queues.builder/src/org/eclipse/scanning/event/queues/builder/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Packagae containing classes which provide the core functionality for 
+ * building o.e.s.event.queues beans.
+ * 
+ * @author Michael Wharmby
+ *
+ */
+package org.eclipse.scanning.event.queues.builder;
